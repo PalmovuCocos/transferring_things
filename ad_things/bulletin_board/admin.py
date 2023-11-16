15554152128c42ace76d4f2e3124ad304854dacd
PalmovuCocos <cocos.barchuck@yandex.ru>
2023-11-16 17:49:42 +0300

    регистрация модели Announcement в панель регистрации

--- ad_things/bulletin_board/admin.py
+++ ad_things/bulletin_board/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Announcement, Category, Application, Comment
 
-# Register your models here.
+
+class AnnouncementAdmin(admin.ModelAdmin):
+    list_display = ('id', 'announcer', 'category', 'name_thing', 'description', 'photo')
+    list_display_links = ('id', 'name_thing')
+    search_fields = ('category', 'name_thing')
+
+
+admin.site.register(Announcement, AnnouncementAdmin)
