118a6691fea02eccfc7821cee1f8d1e15c621f78
PalmovuCocos <cocos.barchuck@yandex.ru>
2023-11-19 19:34:09 +0300

    создание представления регистрации пользователя

--- ad_things/ad_things/urls.py
+++ ad_things/ad_things/urls.py
@@ -3,13 +3,17 @@ from django.urls import path, include
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
+from bulletin_board.views import RegisterAPI
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('bulletin_board/', include('bulletin_board.urls')),
+    path('', include('bulletin_board.urls')),
     # создание
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     # обновление
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     # проверка
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+
+    path('api/register', RegisterAPI.as_view())
 ]
--- ad_things/bulletin_board/views.py
+++ ad_things/bulletin_board/views.py
@@ -3,8 +3,9 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import *
-from .serializers import AnnouncementSerializer, CommentSerializer, ApplicationSerializer, CategorySerializer
-from .permissions import IsAuthenticatedOrReadOnly, IsCurrentUser, IsCurrentUserOrReadOnly, IsNotOwnerUser
+from .serializers import AnnouncementSerializer, CommentSerializer, ApplicationSerializer, CategorySerializer, \
+    RegisterSerializer, UserSerializer
+from .permissions import IsAuthenticatedOrReadOnly, IsCurrentUser, IsCurrentUserOrReadOnly
 
 
 class AnnouncementAPIView(generics.ListCreateAPIView):
@@ -92,4 +93,18 @@ class ConfirmApplicationAPIView(generics.UpdateAPIView):
 
         else:
             return Response({"message": "failed", "details": serializer.errors})
+
+
+class RegisterAPI(generics.GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args,  **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            "message": "User Created Successfully.  Now perform Login to get your token",
+        })
+
 
