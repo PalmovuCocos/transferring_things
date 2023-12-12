5a6a9d9330b3357b0e5c77462fc5ab9f22c7d102
username <user@name.com>
2023-12-12 11:39:33 +0300

    Докер теперь работает с базой данных

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,21 @@
+# создание образа на основе python
 FROM python:3.9.13
 
+# установка виртуальных переменных
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
+# создание рабочей директории
 WORKDIR /usr/src/transferring_things/ad_things
 
+# выполнение команды
 RUN pip install --upgrade pip
+# копирование и установка requirements
 COPY ./requirements.txt /usr/src/
 RUN pip install -r /usr/src/requirements.txt
 
+# копирование всего проекта
 COPY ./ad_things /usr/src/transferring_things/ad_things
-
+#CMD ["python", "manage.py", "migrate"]
+#EXPOSE 8000
+#CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
--- ad_things/ad_things/settings.py
+++ ad_things/ad_things/settings.py
@@ -80,9 +80,12 @@ WSGI_APPLICATION = 'ad_things.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('POSTGRES_NAME'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'NAME': 'transferring_things',
+        'USER': 'dmitry',
+        'PASSWORD': '4221',
+        # 'NAME': os.environ.get('POSTGRES_NAME'),
+        # 'USER': os.environ.get('POSTGRES_USER'),
+        # 'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': 'pgdb',
         'PORT': '5432',
     }
